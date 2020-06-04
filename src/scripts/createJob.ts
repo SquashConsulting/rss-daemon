f435901ba9f6dc6f71b7a85d0ded93067ebdaa18
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-06-04 22:57:14 +0400

    Add RSS Feed recurrent parsing script

--- src/scripts/createJob.ts
+++ src/scripts/createJob.ts
@@ -6,7 +6,14 @@ const feed = argv[0];
 const { QUEUE_NAME, MOUNT_PATH } = module.context.configuration;
 const QUEUE = queues.get(QUEUE_NAME);
 
-QUEUE.push({ mount: MOUNT_PATH, name: 'update-feed' }, feed, {
-  repeatTimes: 3,
-  repeatDelay: 5 * 1000,
+const ONE_HOUR = 3600 * 1000;
+
+const currentDate = new Date();
+const DATE_IN_ONE_HOUR = currentDate.setHours(currentDate.getHours() + 1);
+
+QUEUE.push({ mount: MOUNT_PATH, name: 'update-feed', maxFailures: 2 }, feed, {
+  maxFailures: 2,
+  repeatTimes: Infinity,
+  repeatDelay: ONE_HOUR,
+  delayUntil: DATE_IN_ONE_HOUR,
 });
--- src/scripts/updateFeed.ts
+++ src/scripts/updateFeed.ts
@@ -1,4 +1,17 @@
+import request from '@arangodb/request';
+
 const { argv } = module.context;
 const feed = argv[0];
 
-console.info('FEED IS:', feed);
+const { BACKEND_URL } = module.context.configuration;
+const PARSER_URL = `${BACKEND_URL}/feeds/${feed._key}/parse`;
+
+const response = request({
+  json: true,
+  method: 'PUT',
+  url: PARSER_URL,
+});
+
+if (response.statusCode >= 400) {
+  throw `Could Not Update Feed: [${feed._id}]`;
+}
